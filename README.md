086bf8e30fe83e7fea7b3dbee70c3e0caa48db09
Michael Herrera <52046844+michaelmherrera@users.noreply.github.com>
2020-05-01 19:57:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SututorLink
+## This extension applies soley for tutors and LCPs at Stanford University. It opens the Sututor calendar for either the current week or coming week.
---
+++ popup/get_calendar.js
@@ -0,0 +1,39 @@
+const url_base = "https://sututor.stanford.edu/TracWeb40/schedule.4sp?calStartDate=";
+
+function open_tab(when){
+    /**
+     * Given a datetime, opens the TutorTrac calendar for that date
+     */
+    var date_str = when.toLocaleDateString();
+    var calendar_url = "".concat(url_base, date_str);
+    // First, goes to the tutor profile view. Ommitting this can cause login errors
+    var new_tab = browser.tabs.create({
+        url: "https://sututor.stanford.edu/TracWeb40/ajxData.4sp?type=switchProfile&dir=sc"
+    })
+    // Now, goes to the calendar
+    new_tab.then( function(val){ 
+        browser.tabs.update({url: calendar_url});
+    });
+}
+
+function open_today(today){
+    /**
+     * Open the TutorTrac calendar for the current week
+     */
+    var today = new Date();
+    open_tab(today);
+}
+
+function open_next_week(today){
+    /**
+     * Open the TutorTrac calendar for the coming week
+     */
+    var today = new Date();
+    var next_week = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
+    open_tab(next_week)
+}
+
+var today_button = document.getElementById("today");
+var next_week_button = document.getElementById("next_week");
+next_week_button.addEventListener("click", open_next_week);
+today_button.addEventListener("click", open_today);
